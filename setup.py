77a664dd879e933a1c709d1ba72c71043d0fbddc
LocalCascadeEnsemble <kfauvel.lce@gmail.com>
2022-05-14 00:14:01 +0200

    -

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://lce.readthedocs.io/en/latest/'
 LICENSE = 'Apache-2.0'
 DOWNLOAD_URL = 'https://github.com/LocalCascadeEnsemble/LCE'
 VERSION = __version__
-INSTALL_REQUIRES = ['hyperopt==0.2.7', 'numpy==1.21.5', 'pandas==1.3.5', 'scikit-learn==1.0.2', 'xgboost==1.5.1']
+INSTALL_REQUIRES = ['hyperopt==0.2.7', 'numpy==1.21.5', 'pandas==1.3.5', 'scikit-learn==1.0.2', 'xgboost==1.5.0']
 PROJECT_URLS = {
     "Documentation": "https://lce.readthedocs.io/en/latest/",
 }
